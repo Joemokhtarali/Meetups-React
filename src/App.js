5d21bfbaa14d6dbc4a35c2bb83571bd11e2c56bd
joemokhtarali@gmail.com <joemokhtarali@gmail.com>
2021-04-04 10:43:32 -0400

    added routes using Route & Switch

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
-import './App.css';
-import AllMeetups from './pages/AllMeetups';
-import Favorites from './pages/Favorites';
-import NewMeetup from './pages/NewMeetup';
-
+import "./App.css";
+import { Route, Switch } from "react-router-dom";
+import AllMeetups from "./pages/AllMeetups";
+import Favorites from "./pages/Favorites";
+import NewMeetup from "./pages/NewMeetup";
 
 function App() {
   return (
     <div className="App">
-      <AllMeetups />
-      <Favorites />
-      <NewMeetup />
+      {/* in case I want to add props to component wrap it within route */}
+      <Switch>
+        <Route exact path="/" component={AllMeetups} />
+        <Route path="/Favorites" component={Favorites} />
+        <Route path="/NewMeetup" component={NewMeetup} />
+      </Switch>
     </div>
   );
 }
