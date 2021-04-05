a7ffb0c261a099578e832d40f55bffe27d833120
joemokhtarali@gmail.com <joemokhtarali@gmail.com>
2021-04-05 16:53:36 -0400

    useEffect to get request to firebase

--- src/pages/MeetupList.js
+++ src/pages/MeetupList.js
@@ -1,11 +1,44 @@
 import MeetupItem from "./MeetupItem";
 import classes from "../style/MeetupList.module.css";
+import { useEffect, useState } from "react";
 
 export default function MeetupList(props) {
+  const [isLoading, setIsLoading] = useState(true);
+  const [meetups, setMeetups] = useState([]);
 
-    return (
-        <ul className={classes.list}>
-            {props.meetups.map((meetup, index) => <MeetupItem key={index} {...meetup}/>)}
-        </ul>
+  useEffect(() => {
+    fetch(
+      "https://react-meetup-59fc3-default-rtdb.firebaseio.com/meetups.json",
+      {
+        method: "GET",
+      }
     )
+      .then((res) => res.json())
+      .then((data) => {
+        const updatedMeetups = [];
+
+        for (const key in data) {
+          const meetup = {
+            id: key,
+            ...data[key],
+          };
+          updatedMeetups.push(meetup)
+        }
+
+        setIsLoading(false);
+        setMeetups(updatedMeetups);
+      });
+  }, []);
+
+  if (isLoading) {
+    return <div>Loading</div>;
+  } else {
+    return (
+      <ul className={classes.list}>
+        {meetups.map((meetup, index) => (
+          <MeetupItem key={index} {...meetup} />
+        ))}
+      </ul>
+    );
+  }
 }
--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,55 +1,67 @@
 import classes from "../style/NewMeetupForm.module.css";
 import Card from "../ui/Card";
-import { useRef } from 'react'
+import { useRef } from "react";
+import { useHistory } from 'react-router-dom'
 
 function NewMeetup(props) {
-    const titleRef = useRef()
-    const imageRef = useRef()
-    const addressRef = useRef()
-    const descriptionRef = useRef()
+  const history = useHistory();
+  const titleRef = useRef();
+  const imageRef = useRef();
+  const addressRef = useRef();
+  const descriptionRef = useRef();
 
-    function submitHandler(e) {
-        e.preventDefault()
+  function submitHandler(e) {
+    e.preventDefault();
 
-        const enteredTitle = titleRef.current.value;
-        const enteredImage = imageRef.current.value;
-        const enteredAddress = addressRef.current.value;
-        const enteredDescription = descriptionRef.current.value;
-        
-        const meetupData = {
-            title: enteredTitle,
-            image: enteredImage,
-            address: enteredAddress,
-            description: enteredDescription
-        }
+    const enteredTitle = titleRef.current.value;
+    const enteredImage = imageRef.current.value;
+    const enteredAddress = addressRef.current.value;
+    const enteredDescription = descriptionRef.current.value;
 
-        fetch('https://react-meetup-59fc3-default-rtdb.firebaseio.com/meetups.json', {
-            method: 'POST',
-            body: JSON.stringify(meetupData),
-            headers: {
-                'content-Type': 'application/json'
-            }
-        })
-    }
+    const meetupData = {
+      title: enteredTitle,
+      image: enteredImage,
+      address: enteredAddress,
+      description: enteredDescription,
+    };
+
+    fetch(
+      "https://react-meetup-59fc3-default-rtdb.firebaseio.com/meetups.json",
+      {
+        method: "POST",
+        body: JSON.stringify(meetupData),
+        headers: {
+          "content-Type": "application/json",
+        },
+      }
+    ).then(() => {
+      history.replace('/');
+    });
+  }
 
   return (
     <Card>
-      <form className={classes.form} onSubmit={submitHandler} >
+      <form className={classes.form} onSubmit={submitHandler}>
         <div className={classes.control}>
           <label htmlFor="title">Meetup Title</label>
-          <input type="text" required id="title" ref={titleRef}/>
+          <input type="text" required id="title" ref={titleRef} />
         </div>
         <div className={classes.control}>
           <label htmlFor="image">Meetup Image</label>
-          <input type="url" required id="image" ref={imageRef}/>
+          <input type="url" required id="image" ref={imageRef} />
         </div>
         <div className={classes.control}>
           <label htmlFor="address">Meetup Address</label>
-          <input type="text" required id="address" ref={addressRef}/>
+          <input type="text" required id="address" ref={addressRef} />
         </div>
         <div className={classes.control}>
           <label htmlFor="description">Meetup Description</label>
-          <textarea id="description" required rows="5" ref={descriptionRef}></textarea>
+          <textarea
+            id="description"
+            required
+            rows="5"
+            ref={descriptionRef}
+          ></textarea>
         </div>
         <div className={classes.actions}>
           <button>Add Event</button>
