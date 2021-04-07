10a1311ef70c74156010fde4e047da64f2bd60c6
joemokhtarali@gmail.com <joemokhtarali@gmail.com>
2021-04-07 12:24:18 -0400

    added favorites & last commit in class

--- src/pages/AllMeetups.js
+++ src/pages/AllMeetups.js
@@ -1,27 +1,5 @@
 import MeetupList from './MeetupList'
 
-
-const DUMMY_DATA = [
-    {
-      id: 'm1',
-      title: 'This is a first meetup',
-      image:
-        'https://ychef.files.bbci.co.uk/624x351/p07zy3y6.jpg',
-      address: 'Zamalik, Downtown Cairo, Egypt',
-      description:
-        'This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!',
-    },
-    {
-      id: 'm2',
-      title: 'This is a second meetup',
-      image:
-        'https://ychef.files.bbci.co.uk/624x351/p07zy3y6.jpg',
-      address: 'Zamalik, Downtown Cairo, Egypt',
-      description:
-        'This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!',
-    },
-  ];
-
 function AllMeetups(props) {
 
     return (
@@ -29,7 +7,7 @@ function AllMeetups(props) {
             
             <h1>All Meetups</h1>
             <ul>
-                <MeetupList meetups={DUMMY_DATA}/>
+                <MeetupList meetups={props}/>
             </ul>
         </div>
     )
--- src/pages/Favorites.js
+++ src/pages/Favorites.js
@@ -1,8 +1,16 @@
-function Favorites(props) {
+import { useContext } from "react";
+import FavoritesContext from "../store/favorites-context";
+import MeetupList from "./MeetupList";
 
-    return (
-        <div>Favorites</div>
-    )
+function Favorites(props) {
+  const favoriteCtx = useContext(FavoritesContext);
+
+  return (
+    <section>
+      <h1>Favorites</h1>
+      <MeetupList meetups={favoriteCtx.favorites}/>
+    </section>
+  );
 }
 
-export default Favorites
+export default Favorites;
